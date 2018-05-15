c7a5fb71f5e8de4b5a557fad694e6d51520df779
Mathew Brown <matt@mattbrown.com>
2018-05-15 08:36:48 -0600

    Testing app implementation and tests

--- testing/src/components/app.js
+++ testing/src/components/app.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
-
+import CommentBox from '../components/comment_box'
+import CommentList from '../components/comment_list'
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <CommentBox />
+        <CommentList />
+      </div>
     );
   }
 }
