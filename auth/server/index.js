3c3c5a58bbcf8336da87b636993512130d0c7275
Mathew Brown <matbrown@fanatics.com>
2018-06-01 07:56:57 -0600

    Complete Server and Start Client App

--- auth/server/index.js
+++ auth/server/index.js
@@ -2,24 +2,22 @@ const express = require('express')
 const http = require('http')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
-const mongoose = require('mongoose')
-
-const router = require('./router')
-
 const app = express()
+const router = require('./router')
+const mongoose = require('mongoose')
+const cors = require('cors')
 
 // DB Setup
-
-mongoose.connect('mongodb://localhost:27017/auth').catch(console.log)
+mongoose.connect('mongodb://localhost/auth')
 
 // App Setup
 app.use(morgan('combined'))
-app.use(bodyParser.json({type: '*/*'}))
+app.use(cors())
+app.use(bodyParser.json({ type: '*/*' }))
 router(app)
 
 // Server Setup
-
 const port = process.env.PORT || 3000
 const server = http.createServer(app)
-server.listen(port) 
+server.listen(port)
 console.log('Server listening on:', port)
