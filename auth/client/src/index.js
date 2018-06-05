3c98ea0ee78a866d5ad0c9310602cd1748fe465d
Mathew Brown <matbrown@fanatics.com>
2018-06-05 15:57:56 -0600

    Begin client Implement Signup

--- auth/client/src/index.js
+++ auth/client/src/index.js
@@ -1,15 +1,31 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import { BrowserRouter, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import reduxThunk from 'redux-thunk'
 
 import App from './components/App'
+
+import Signup from './components/auth/Signup'
 import Welcome from './components/Welcome'
 
+import reducers from './reducers'
+
+const store = createStore(
+  reducers,
+  {},
+  applyMiddleware(reduxThunk)
+)
+
 ReactDom.render(
-  <BrowserRouter>
-    <App>
-      <Route path="/" exact component={Welcome} /> 
-    </App>
-  </BrowserRouter>,
+  <Provider store={store}>    
+    <BrowserRouter>
+      <App>
+        <Route path="/" exact component={Welcome} /> 
+        <Route path="/signup" component={Signup} />
+      </App>
+    </BrowserRouter>
+  </Provider>,
   document.querySelector('#root')
 ) 
